16a04b33442e6c032c98ae8ae10c2bace6c09d55
Craig Barry <craigfbarry@gmail.com>
2020-07-17 18:20:43 +0930

    Added Search button and hooks.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,57 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Input from "./components/Input";
 import { Container, Row, Col } from "./components/Grid";
 import "./App.css";
+import SearchButton from "./components/SearchButton";
+import API from "./utils/API";
 
 function App() {
+
+    const [books, setBooks] = useState([]);
+    const [bookSearch,setBookSearch] = useState("");
+
+    const handleInputChange = event => {
+        const {value} = event.target;
+        setBookSearch(value);
+        console.log(value)
+    }
+
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        API.getBooks(BookSearch)
+          .then(res => setBooks(res.data))
+          .catch(err => console.log(err));
+    };
+
   return (
     <div className="App">
       <Header/>  
       <Container>
         <Search/>
-        <Input
-        name="BookSearch"
-        placeholder="Search For a Book"
-        />
+          <Row>
+            <Col size="xs-8 sm-8">
+              <Input
+              value={bookSearch}
+                onChange={handleInputChange}
+                name="BookSearch"
+                placeholder="Search For a Book"
+                />
+            </Col>
+            <Col size="xs-3 sm-2">
+              <SearchButton
+                onClick={handleFormSubmit}
+                type="success"
+                className="input-lg"
+              >
+                Search
+              </SearchButton>
+                
+                
+            </Col>
+          </Row>
       </Container> 
     </div>
   );
--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./style.css";
 
 function Search(){
     return(
         <div className="justify-content-left">
             <h4>Book Search</h4>
-            <h6>Book</h6>
         </div>
     )
 }
