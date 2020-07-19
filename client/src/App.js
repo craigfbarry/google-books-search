670443ad323c026eac08c771a97c61e5ec41c7fb
Craig Barry <craigfbarry@gmail.com>
2020-07-19 12:42:36 +0930

    Added React router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,24 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import Input from "./components/Input";
-import { Container, Row, Col } from "./components/Grid";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Wrapper from "./components/Wrapper"
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved"
 import "./App.css";
-import SearchButton from "./components/SearchButton";
-import API from "./utils/API";
-import { BookList, BookListItem } from "./components/BookList";
+
 
 function App() {
 
-    const [books, setBooks] = useState([]);
-    const [bookSearch,setBookSearch] = useState("");
-
-    const handleInputChange = event => {
-        const {value} = event.target;
-        setBookSearch(value);
-    }
-
-
-    const handleFormSubmit = event => {
-        event.preventDefault();
-        API.getBooks(bookSearch)
-          .then(res => {setBooks(res.data.items)
-              console.log(res.data.items)
-          })
-          .catch(err => console.log(err));
-    };
-
   return (
-    <div className="App">
-      <Header/>  
-      <Container>
-        <Search/>
-          <Row className="mb-3">
-            <Col size="xs-8 sm-8">
-              <Input
-               name="BookSearch"
-                value={bookSearch}
-                onChange={handleInputChange}
-                placeholder="Search For a Book Title"
-                />
-            </Col>
-            <Col size="xs-3 sm-2">
-              <SearchButton
-                onClick={handleFormSubmit}
-                type="success"
-                className="input-lg"
-              >
-                Search
-              </SearchButton>                        
-            </Col>
-          </Row>
-          <Row>
-            <Col size="xs-12">
-                <BookList>
-                    {books.map(book => {
-                        return (
-                            <BookListItem
-                                key={book.volumeInfo.id}
-                                title={book.volumeInfo.title}
-                                authors={book.volumeInfo.authors}
-                                description={book.volumeInfo.description}
-                                link={book.volumeInfo.selfLink}
-                                image={book.volumeInfo.imageLinks.thumbnail}
-                            />
-                        );
-                    })}
-                </BookList>
-            </Col>       
-          </Row>                      
-      </Container> 
-    </div>
+      <Router>
+          <div>
+            <Nav/>
+              <Wrapper>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/Saved" component={Saved} />
+              </Wrapper>
+          </div>
+      </Router>    
   );
 }
 
